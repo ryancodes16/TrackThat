8c387b0d664c7641396363a0e0b0453dfb4e3615
Ryan <ryancodes16@gmail.com>
2017-08-19 10:35:56 -0500

    Add files via upload
    Setting up my project

---
+++ TrackThat/Driver.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+/**
+ * Intializes the employee program
+ * 
+ * @author Ryan 
+ * @version 1.0
+ */
+public class Driver
+{
+    private static String companyName;
+    private static int numEmployees;
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
---
+++ TrackThat/Employee.java
@@ -0,0 +1,14 @@
+
+public class Employee
+{
+    private String first, last, position;
+    private double hourlyRate;
+    public Employee(String firstName, String lastName, String position, double hourlyRate)
+    {
+        this.first = firstName;
+        this.last = lastName;
+        this.position = position;
+        this.hourlyRate = hourlyRate;
+    }
+    
+}
